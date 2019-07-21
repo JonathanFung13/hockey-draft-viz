bcf07efa3e7c62de8f9370df20a2281df86abc8a
Jonathan Fung <jfung9@gatech.edu>
2019-07-21 14:11:50 -0700

    Bumping up font in header and nav again.

--- js/main.js
+++ js/main.js
@@ -13,11 +13,11 @@ const numDraftRounds = 7;
 
 const legendKey = {};
 legendKey['status'] = {class: {"active": "active", "other_team": "other_team", "inactive": "inactive"},
-    text: {"active": ["On roster", 90], "other_team": ["Other teams roster", 110], "inactive": ["Inactive", 140]}};
+    text: {"active": ["On roster", 90], "other_team": ["Other teams roster", 140], "inactive": ["Inactive", 190]}};
 legendKey['gpClass'] = {class: {4: "four", 3: "three", 2: "two", 1: "one"},
-    text: {4: [">60", 90], 3: ["40-60", 90], 2: ["20-40", 90], 1: ["0-20", 120]} };
+    text: {4: [">60", 110], 3: ["40-60", 110], 2: ["20-40", 110], 1: ["0-20", 110]} };
 legendKey['ppgClass'] = {class: {1: "one", 2: "two", 3: "three", 4: "four", 5: "five"},
-    text: {1: ["0.0-0.25", 120], 2: ["0.25-0.50", 110],3: ["0.50-0.75", 110], 4: ["0.75-1.0", 110], 5: [">1.0", 110]} };
+    text: {1: ["0.0-0.25", 120], 2: ["0.25-0.50", 140],3: ["0.50-0.75", 150], 4: ["0.75-1.0", 150], 5: [">1.0", 150]} };
 
 // Variables
 let clickedDict = {"active": false, "inactive": false, "other_team": false};
@@ -52,7 +52,7 @@ async function displayTeams() {
             let borderParams = $(this).offset();
             d3.select("#selectedBorder > rect")
                 .attr("x", borderParams.left - 15) // + $(window).scrollLeft())
-                .attr('y', borderParams.top - 47);
+                .attr('y', borderParams.top - 77);
             displayTeamDetail(picksByTeam, teamName, svgHolder)
         });
         plotDraftPicks(picksByTeam, previewSvg, PREVIEW_SIZE)
